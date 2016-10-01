dc038bbd73e895ea96b576059db97953c824e4b5
mk43 <1801598925@qq.com>
2016-10-01 15:13:57 +0800

    增加各个省市县的信息

--- app/build/generated/source/r/debug/com/coolweather/R.java
+++ app/build/generated/source/r/debug/com/coolweather/R.java
@@ -1775,7 +1775,7 @@ containing a value of this type.
         public static final int notification_template_icon_bg=0x7f020050;
     }
     public static final class id {
-        public static final int action0=0x7f0b0055;
+        public static final int action0=0x7f0b0056;
         public static final int action_bar=0x7f0b0045;
         public static final int action_bar_activity_content=0x7f0b0000;
         public static final int action_bar_container=0x7f0b0044;
@@ -1784,23 +1784,22 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f0b0025;
         public static final int action_bar_title=0x7f0b0024;
         public static final int action_context_bar=0x7f0b0046;
-        public static final int action_divider=0x7f0b0059;
+        public static final int action_divider=0x7f0b005a;
         public static final int action_menu_divider=0x7f0b0002;
         public static final int action_menu_presenter=0x7f0b0003;
         public static final int action_mode_bar=0x7f0b0042;
         public static final int action_mode_bar_stub=0x7f0b0041;
         public static final int action_mode_close_button=0x7f0b0026;
         public static final int activity_chooser_view_content=0x7f0b0027;
-        public static final int activity_main=0x7f0b0054;
         public static final int add=0x7f0b0013;
         public static final int alertTitle=0x7f0b0033;
         public static final int always=0x7f0b001d;
         public static final int beginning=0x7f0b001a;
         public static final int bottom=0x7f0b0022;
         public static final int buttonPanel=0x7f0b002e;
-        public static final int cancel_action=0x7f0b0056;
+        public static final int cancel_action=0x7f0b0057;
         public static final int checkbox=0x7f0b003c;
-        public static final int chronometer=0x7f0b005c;
+        public static final int chronometer=0x7f0b005d;
         public static final int collapseActionView=0x7f0b001e;
         public static final int contentPanel=0x7f0b0034;
         public static final int custom=0x7f0b003a;
@@ -1810,7 +1809,7 @@ containing a value of this type.
         public static final int disableHome=0x7f0b000c;
         public static final int edit_query=0x7f0b0047;
         public static final int end=0x7f0b001b;
-        public static final int end_padder=0x7f0b0061;
+        public static final int end_padder=0x7f0b0062;
         public static final int expand_activities_button=0x7f0b0028;
         public static final int expanded_menu=0x7f0b003b;
         public static final int home=0x7f0b0004;
@@ -1818,12 +1817,13 @@ containing a value of this type.
         public static final int icon=0x7f0b002c;
         public static final int ifRoom=0x7f0b001f;
         public static final int image=0x7f0b0029;
-        public static final int info=0x7f0b0060;
-        public static final int line1=0x7f0b005a;
-        public static final int line3=0x7f0b005e;
+        public static final int info=0x7f0b0061;
+        public static final int line1=0x7f0b005b;
+        public static final int line3=0x7f0b005f;
         public static final int listMode=0x7f0b0009;
         public static final int list_item=0x7f0b002b;
-        public static final int media_actions=0x7f0b0058;
+        public static final int list_view=0x7f0b0055;
+        public static final int media_actions=0x7f0b0059;
         public static final int middle=0x7f0b001c;
         public static final int multiply=0x7f0b0014;
         public static final int never=0x7f0b0020;
@@ -1857,16 +1857,17 @@ containing a value of this type.
         public static final int src_atop=0x7f0b0016;
         public static final int src_in=0x7f0b0017;
         public static final int src_over=0x7f0b0018;
-        public static final int status_bar_latest_event_content=0x7f0b0057;
+        public static final int status_bar_latest_event_content=0x7f0b0058;
         public static final int submenuarrow=0x7f0b003f;
         public static final int submit_area=0x7f0b0050;
         public static final int tabMode=0x7f0b000b;
-        public static final int text=0x7f0b005f;
-        public static final int text2=0x7f0b005d;
+        public static final int text=0x7f0b0060;
+        public static final int text2=0x7f0b005e;
         public static final int textSpacerNoButtons=0x7f0b0037;
-        public static final int time=0x7f0b005b;
+        public static final int time=0x7f0b005c;
         public static final int title=0x7f0b002d;
         public static final int title_template=0x7f0b0032;
+        public static final int title_text=0x7f0b0054;
         public static final int top=0x7f0b0023;
         public static final int topPanel=0x7f0b0031;
         public static final int up=0x7f0b0008;
@@ -1879,6 +1880,7 @@ containing a value of this type.
         public static final int abc_config_activityShortDur=0x7f0c0001;
         public static final int cancel_button_image_alpha=0x7f0c0002;
         public static final int status_bar_notification_info_maxnum=0x7f0c0003;
+        public static final int version=0x7f0c0004;
     }
     public static final class layout {
         public static final int abc_action_bar_title_item=0x7f040000;
@@ -1907,7 +1909,7 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f040017;
         public static final int abc_search_view=0x7f040018;
         public static final int abc_select_dialog_material=0x7f040019;
-        public static final int activity_main=0x7f04001a;
+        public static final int choose_area=0x7f04001a;
         public static final int notification_media_action=0x7f04001b;
         public static final int notification_media_cancel_action=0x7f04001c;
         public static final int notification_template_big_media=0x7f04001d;
@@ -1957,6 +1959,8 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f060011;
         public static final int abc_toolbar_collapse_description=0x7f060012;
         public static final int app_name=0x7f060021;
+        public static final int china=0x7f060022;
+        public static final int db_name=0x7f060023;
         public static final int search_menu_title=0x7f060013;
         public static final int status_bar_notification_info_overflow=0x7f060014;
     }
